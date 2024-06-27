74e702bcae968ea20a23d11d5b5d18e51dfa4f71
Caleb <cjschaun@gmail.com>
2024-06-27 15:27:36 -0500

    Fixing cors

--- app.js
+++ app.js
@@ -16,18 +16,18 @@ const pool = mysql.createPool({
 }).promise();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 
-const allowedOrigins = ['https://editor.wix.com', 'https://ncpaofficial.com', 'https://www.ncpaofficial.com'];
-app.use(cors({
-    origin: function(origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
+// const allowedOrigins = ['https://editor.wix.com', 'https://ncpaofficial.com', 'https://www.ncpaofficial.com'];
+// app.use(cors({
+//     origin: function(origin, callback) {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     }
+// }));
 
 const port = 3000;
 
