1c521f7dd5e7eceebd697f34d26f0ec34d902909
Caleb <cjschaun@gmail.com>
2024-08-02 12:56:48 -0500

    update college rankings request

--- app.js
+++ app.js
@@ -2848,6 +2848,7 @@ async function updateCollegeRankings() {
             // console.log(team, tournament_teams[team].rank);
             try {
                 await pool.query(`UPDATE College SET ranking=${tournament_teams[team].rank} WHERE name="${team}"`);
+                console.log(rank)
             } catch (error) {
                 try {
                     await pool.query(`INSERT INTO College(name, ranking) VALUES("${team}", ${tournament_teams[team].rank})`);
