b9ae39a8cdc991ef155c0ff30a97bba0ef57d525
Caleb <cjschaun@gmail.com>
2024-08-02 12:41:39 -0500

    chaned base url

--- app.js
+++ app.js
@@ -108,7 +108,8 @@ function authenticate(req, res, next) {
 
 // updateCollegeRankings();
 
-let base_url = 'http://localhost:3000';
+// let base_url = 'http://localhost:3000';
+let base_url = 'https://api.ncpaofficial.com'
 
 // Routes
 
